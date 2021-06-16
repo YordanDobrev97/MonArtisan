dab5ff327a1e9e4164df913dddc6818e2ed203ff
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-06-16 18:24:00 +0300

    Add Project Entity

---
+++ Data/MonArtisan.Data.Models/Project.cs
@@ -0,0 +1,22 @@
+﻿
+namespace MonArtisan.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Project
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public int CustomID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Craftsman { get; set; }
+
+        public bool State { get; set; }
+    }
+}
