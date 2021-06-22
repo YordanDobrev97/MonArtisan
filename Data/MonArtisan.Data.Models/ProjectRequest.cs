deb86eef8ad88506e9a29cc9897f72959155da79
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-06-22 18:12:04 +0300

    Add Approved Property of ProjectRequest

--- Data/MonArtisan.Data.Models/ProjectRequest.cs
+++ Data/MonArtisan.Data.Models/ProjectRequest.cs
@@ -18,6 +18,8 @@
 
         public decimal Price { get; set; }
 
+        public bool Approved { get; set; }
+
         public bool IsDeleted { get; set; }
 
         public DateTime? DeletedOn { get; set; }
